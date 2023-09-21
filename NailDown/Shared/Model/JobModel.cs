1ddc00d8cbe1379cf6f086d31743c48434a7e222
Sinan <devsinan@tutamail.com>
2023-09-21 13:30:57 +0200

    Added "JobModel.cs"

---
+++ NailDown/Shared/Model/JobModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NailDown.Shared.Model {
+    public class JobModel {
+        [Key]
+        public uint Id { get; set; }
+        [Required]
+        public string Title { get; set; } = "";
+        public string? Description { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public TimeSpan RelativeTime { get => DateTime.Now - CreatedDate; }
+    }
+}
