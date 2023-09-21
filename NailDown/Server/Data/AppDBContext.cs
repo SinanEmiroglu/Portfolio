56a51d519f88dbea19d0d43b3c36a278ee991abc
Sinan <devsinan@tutamail.com>
2023-09-21 13:38:33 +0200

    Added "AppDBContext.cs"

---
+++ NailDown/Server/Data/AppDBContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NailDown.Server.Data {
+    public class AppDBContext : DbContext {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) {
+
+        }
+    }
+}
