57598ebe8b4185be14cbf0dcb3ff12c7b12be525
Sinan <devsinan@tutamail.com>
2023-09-21 13:41:27 +0200

    Added "AppDBContext" to the services

--- NailDown/Server/Program.cs
+++ NailDown/Server/Program.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using NailDown.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDBContext>(opt => {
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
